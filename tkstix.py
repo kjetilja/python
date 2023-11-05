ffa978fdea4f40b5a63c2fb8e94cad0dd97e5ac7
Kjetil Jacobsen <kjetilja@gmail.com>
2023-11-05 17:38:54 +0100

    When drawing, continue drawing in the same direction as the player previously moved. Cleanups to the internal naming conventions.

--- tkstix.py
+++ tkstix.py
@@ -2,18 +2,19 @@ import tkinter as tk
 import random
 
 class Path(object):
+    """Track the positions that are being drawed."""
     def __init__(self, arena_width, arena_height):
         self.arena_width = arena_width
         self.arena_height = arena_height
         self.reset()
 
     def reset(self):
-        self.points = []
+        self.positions = []
 
-    def add_point(self, x, y):
-        self.points.append((x,y))
+    def add_position(self, x, y):
+        self.positions.append((x,y))
 
-    def get_free_point(self, arena):
+    def get_free_position(self, arena):
         for y in range(0, self.arena_height):
             for x in range(0, self.arena_width):
                 if arena[(y * self.arena_width)+x] == 0:
@@ -30,6 +31,10 @@ class Arena(object):
         self.arena_ypos = 0
         self.is_drawing = False
         self.drawing_completed = False
+        self.initialize_arena_state()
+
+    def initialize_arena_state(self):
+        """Create the arena game state, a rectangle with 1's (wall) filled with 0's."""
         frame = [1 for x in range(self.arena_width)]
         self.arena += frame
         walls = [1] + [0] * (self.arena_width-2) + [1]
@@ -38,20 +43,20 @@ class Arena(object):
         self.arena += frame
 
     def can_move_horizontally(self, xadjust):
+        """Check whether dot can be moved in horizontally by xadjust, return True if so, False otherwise."""
         if (self.arena_xpos + xadjust < 0) or (self.arena_xpos + xadjust >= self.arena_width):
             return False 
         arena_position = (self.arena_ypos * self.arena_width) + (self.arena_xpos + xadjust)
-        print(self.arena[arena_position])
         if self.is_drawing:
             if self.arena[arena_position] == 0:
-                self.path.add_point(self.arena_xpos + xadjust, self.arena_ypos)
+                self.path.add_position(self.arena_xpos + xadjust, self.arena_ypos)
                 self.arena[arena_position] = 2
                 return True
-            elif self.arena[arena_position] == 1 and len(self.path.points) > 2:
+            elif self.arena[arena_position] == 1 and len(self.path.positions) > 2:
                 self.drawing_completed = True
                 return True
             elif self.arena[arena_position] == 2:
-                print("FAIL")
+                print("FAIL") # TODO: lose a life
                 return False
         else:
             if self.arena[arena_position] == 1:
@@ -59,20 +64,20 @@ class Arena(object):
         return False
 
     def can_move_vertically(self, yadjust):
+        """Check whether dot can be moved in vertically by yadjust, return True if so, False otherwise."""
         if (self.arena_ypos + yadjust < 0) or (self.arena_ypos + yadjust >= self.arena_height):
             return False
         arena_position = ((self.arena_ypos + yadjust) * self.arena_width) + self.arena_xpos
-        print(self.arena[arena_position])
         if self.is_drawing:
             if self.arena[arena_position] == 0:
-                self.path.add_point(self.arena_xpos, self.arena_ypos + yadjust)
+                self.path.add_position(self.arena_xpos, self.arena_ypos + yadjust)
                 self.arena[arena_position] = 2
                 return True
-            elif self.arena[arena_position] == 1 and len(self.path.points) > 2:
+            elif self.arena[arena_position] == 1 and len(self.path.positions) > 2:
                 self.drawing_completed = True
                 return True
             elif self.arena[arena_position] == 2:
-                print("FAIL")
+                print("FAIL") # TODO: lose a life
                 return False
         else:
             if self.arena[arena_position] == 1:
@@ -83,7 +88,7 @@ class Arena(object):
         """
         After drawing initiated:
             - The next move must be on a value 0 (free) or value 1 (terminate).
-            - During drawing, values under the point are set to 2.
+            - During drawing, values under the current position are set to 2.
             - If attempting move to value 2, lose a life and revert line.
             - Move to value 3 (filled) is considered a bug.
         """
@@ -93,7 +98,7 @@ class Arena(object):
             self.path.reset()
 
     def is_possible_to_draw(self):
-        """ Return True if it is possible to start drawing from this position."""
+        """Return True if it is possible to start drawing from this position."""
         if self.arena_xpos > 0:
             arena_position = (self.arena_ypos * self.arena_width) + self.arena_xpos - 1
             if self.arena[arena_position] == 0: return True
@@ -108,20 +113,20 @@ class Arena(object):
             if self.arena[arena_position] == 0: return True
 
     def fill_arena(self, box_callback):
-        # Find all areas that must be filled
+        """Find all areas in the arena that must be filled"""
         areas = {}
         while True:
-            free_point = self.path.get_free_point(self.arena) 
-            if free_point == None:
+            free_position = self.path.get_free_position(self.arena)
+            if free_position == None:
                 break
-            fill_points = self.fill_region(free_point[0], free_point[1], 3)
-            areas[len(fill_points)] = fill_points
+            fill_positions = self.fill_region(free_position[0], free_position[1], 3)
+            areas[len(fill_positions)] = fill_positions
         area_sizes = list(areas.keys())
         area_sizes.sort()
         print("area sizes", area_sizes)
 
-        # Check that there exist a fillable area - if not, we have exhausted all free slots on the arena
-        if len(area_sizes) > 0:
+        # Check that there exist a fillable area - if not, we have exhausted all free positions on the arena
+        if len(area_sizes) > 0: # TODO: improve to check whether level is completed
             largest_area = area_sizes[-1]
 
             # The largest area is not filled
@@ -130,15 +135,15 @@ class Arena(object):
                 self.arena[arena_position] = 0
 
             # All other areas are filled
-            for fill_area in area_sizes[:-1]:
+            for fill_area in area_sizes[:-1]: # TODO: handle case with area_sizes == 1
                 for x, y in areas[fill_area]:
                     box_callback(x, y)
         else:
-            # No fillable slots remain
-            print("Arena filled")
+            # No fillable position on the arena remain
+            print("Arena filled") # TODO: set state to indicate that level completed
 
-        # Change all the value 2 points on the pathline to value 1 so they can be traversed
-        for x, y in self.path.points:
+        # Change all the value 2 position on the path to value 1 so they can be traversed
+        for x, y in self.path.positions:
             arena_position = y * self.arena_width + x
             if self.arena[arena_position] == 2:
                 self.arena[arena_position] = 1
@@ -147,22 +152,22 @@ class Arena(object):
         self.is_drawing = False
         self.drawing_completed = True
 
-    def fill_region(self, seedx, seedy, point_value):
-        fill_points = []
-        points_list = [(seedx, seedy)]
-        while len(points_list) > 0:
-            x, y = points_list.pop()
+    def fill_region(self, seedx, seedy, position_value):
+        fill_positions = []
+        positions_list = [(seedx, seedy)]
+        while len(positions_list) > 0:
+            x, y = positions_list.pop()
             if x < 0 or x >= self.arena_width or y < 0 or y >= self.arena_height:
                 continue
             arena_position = y * self.arena_width + x
             if self.arena[arena_position] == 0:
-                self.arena[arena_position] = point_value
-                fill_points.append((x, y))
-                points_list.append((x + 1, y))
-                points_list.append((x - 1, y))
-                points_list.append((x, y + 1))
-                points_list.append((x, y - 1))
-        return fill_points
+                self.arena[arena_position] = position_value
+                fill_positions.append((x, y))
+                positions_list.append((x + 1, y))
+                positions_list.append((x - 1, y))
+                positions_list.append((x, y + 1))
+                positions_list.append((x, y - 1))
+        return fill_positions
             
 class Game(object):
     def __init__(self, canvas):
@@ -176,9 +181,10 @@ class Game(object):
         self.xpos = 10
         self.ypos = 10
         self.dot = self.canvas.create_dot(self.xpos, self.ypos)
-        self.game_speed = 50
+        self.game_speed = 40
         self.pixels_per_move = 5
         self.arena = Arena(self.canvas.width-20, self.canvas.height-20, self.pixels_per_move)
+        self.next_move_callback = None
         self.game_loop()
 
     def up(self):
@@ -187,9 +193,12 @@ class Game(object):
             self.canvas.move(self.dot, 0, -self.pixels_per_move)
             self.arena.arena_ypos -= 1
             if self.arena.is_drawing:
+                self.next_move_callback = self.up
                 self.canvas.create_line(self.xpos, self.ypos + self.pixels_per_move, self.xpos, self.ypos)
                 if self.arena.drawing_completed:
                     self.fill_arena()
+            else:
+                self.next_move_callback = None
 
     def down(self):
         if self.arena.can_move_vertically(1):
@@ -197,9 +206,12 @@ class Game(object):
             self.canvas.move(self.dot, 0, self.pixels_per_move)
             self.arena.arena_ypos += 1
             if self.arena.is_drawing:
+                self.next_move_callback = self.down
                 self.canvas.create_line(self.xpos, self.ypos - self.pixels_per_move, self.xpos, self.ypos)
                 if self.arena.drawing_completed:
                     self.fill_arena()
+            else:
+                self.next_move_callback = None
 
     def left(self):
         if self.arena.can_move_horizontally(-1):
@@ -207,9 +219,12 @@ class Game(object):
             self.canvas.move(self.dot, -self.pixels_per_move, 0)
             self.arena.arena_xpos -= 1
             if self.arena.is_drawing:
+                self.next_move_callback = self.left
                 self.canvas.create_line(self.xpos + self.pixels_per_move, self.ypos, self.xpos, self.ypos)
                 if self.arena.drawing_completed:
                     self.fill_arena()
+            else:
+                self.next_move_callback = None
 
     def right(self):
         if self.arena.can_move_horizontally(1):
@@ -217,9 +232,12 @@ class Game(object):
             self.canvas.move(self.dot, self.pixels_per_move, 0)
             self.arena.arena_xpos += 1
             if self.arena.is_drawing:
+                self.next_move_callback = self.right
                 self.canvas.create_line(self.xpos - self.pixels_per_move, self.ypos, self.xpos, self.ypos)
                 if self.arena.drawing_completed:
                     self.fill_arena()
+            else:
+                self.next_move_callback = None
 
     def initiate_drawing(self):
         self.arena.initiate_drawing()
@@ -232,6 +250,8 @@ class Game(object):
         self.level = 1
 
     def game_loop(self):
+        if self.next_move_callback != None:
+            self.next_move_callback()
         self.canvas.invoke_callback(self.game_speed, self.game_loop)
 
 class Canvas(tk.Frame):
